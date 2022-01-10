24ef9daa49aa9600235effdc026b0fb9a77b255a
bao-nguyen-khac <69065936+bao-nguyen-khac@users.noreply.github.com>
2022-01-10 22:32:55 +0700

    add js frontend

---
+++ src/public/js/main.js
@@ -0,0 +1,32 @@
+var btnInfo = document.querySelector('.icon-show-info');
+var userInfo = document.querySelector('.user-profile-sidebar');
+var btnCloseInfo = document.querySelector('.btn-close-user-info');
+var iconCloseInfo = document.querySelector('.icon-close-user-info');
+var btnCloseResponsive = document.querySelector('.btn-close-responsive');
+var listElementChat = document.querySelectorAll('.chat-element-user');
+var listElementMess = document.querySelectorAll('.user-chat-content');
+listElementMess[listElementMess.length - 1].scrollIntoView();
+btnInfo.addEventListener('click', () => {
+    if(userInfo.classList.contains('d-block')){
+        userInfo.classList.remove('d-block')
+    }else{
+        userInfo.classList.add('d-block')
+    }
+})
+btnCloseInfo.addEventListener('click', () => {
+    userInfo.classList.remove('d-block')
+})
+iconCloseInfo.addEventListener('click', () => {
+    userInfo.classList.remove('d-block')
+})
+btnCloseResponsive.addEventListener('click', () => {
+    document.querySelector('.user-chat').classList.remove('user-chat-show');
+})
+listElementChat.forEach(element => {
+
+    element.addEventListener('click', () => {
+        if(!document.querySelector('.user-chat').classList.contains('user-chat-show')){
+            document.querySelector('.user-chat').classList.add('user-chat-show');
+        }
+    })
+});
